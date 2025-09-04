e0d3efa05c72dfa2bb29a5204e00837856350ab1
MarlineIOS <blanxxtygadress@gmail.com>
2025-09-04 07:00:09 +0300

    mini fix

--- apps/web/DevelopmentSucks.WEB/obj/Debug/net9.0/DevelopmentSucks.WEB.AssemblyInfo.cs
+++ apps/web/DevelopmentSucks.WEB/obj/Debug/net9.0/DevelopmentSucks.WEB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevelopmentSucks.WEB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4798fce3071f15fb24793b8a46de4bdad926e92f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f4a7ca95a0d2f492473385e21c164b9d88c68e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevelopmentSucks.WEB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevelopmentSucks.WEB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
